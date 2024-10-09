28f392fd4dbfc76da25ecfe7a9818530cab4796b
Ramane23 <contactsouley@gmail.com>
2024-10-09 09:33:08 +0200

    updates

--- services/flights_with_weather/src/config.py
+++ services/flights_with_weather/src/config.py
@@ -1,9 +1,10 @@
 from pydantic_settings import BaseSettings
 from pydantic import field_validator
+from typing import List, Optional
 
 # Define a class to hold the configuration settings
 class Config(BaseSettings):
-    kafka_broker_address: str
+    kafka_broker_address: Optional[str] = None
     kafka_input_topic_name: str
     kafka_consumer_group: str
     kafka_output_topic_name: str
